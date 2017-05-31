43cab68f8f0936f5b35d0ab3a5c8d6652f046721
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-05-31 16:49:06 -0700

    Moved files around.

--- rcresatjob.sh
+++
@@ -1,2 +0,0 @@
-bash /home/jdellit/scripts/rcreserve.sh
-at -f /home/jdellit/scripts/rcresatjob.sh now + 1 day
--- rcreserve.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rcres unlease rc01-10 
-for i in {01..10}
-do
-  read line
-  rcres lease 1d rc$i -m "$line"
-done < /home/jdellit/scripts/rcresbanner$(($RANDOM % 8)).txt
